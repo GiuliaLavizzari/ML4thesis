e9b7884e93ac354117f4716ea635f7a315279663
GiuliaLavizzari <83579300+GiuliaLavizzari@users.noreply.github.com>
2021-08-04 20:30:56 +0200

    job (all operators)

---
+++ job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+stringOP="cHbox cHDD cHl1 cHl3 cHq1 cHq3 cHWB cHW cll1 cll cqq11 cqq1 cqq31 cqq3"
+
+python lossperbatch.py 1 7
+echo "done with SM"
+
+for oper in $stringOP; do
+    python lossperbatchBSM.py 1 7 $oper
+    python finalBSM.py 1 7 $oper > output${oper}_m1_d7.txt
+    echo "done with $oper"
+done  
+    
+
+
