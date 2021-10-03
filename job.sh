3a61990021a893f2ebd88d5bdae61e7320a9e43e
GiuliaLavizzari <83579300+GiuliaLavizzari@users.noreply.github.com>
2021-10-03 18:33:42 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -7,7 +7,7 @@ echo "done with SM"
 
 for oper in $stringOP; do
     python lossperbatchBSM.py 1 7 $oper
-    python finalBSM.py 1 7 $oper > output${oper}_m1_d7.txt
+    python finalBSM1.py 1 7 $oper > output${oper}_m1_d7.txt
     echo "done with $oper"
 done  
     
